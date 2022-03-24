cd60e1c82d5877153d41cc513127b9de4c623e13
Nahzad Faruk <100996758+nahzad@users.noreply.github.com>
2022-03-24 19:50:42 +0600

    Class 2 Assignment Find Possible variable done

---
+++ find_variable.php
@@ -0,0 +1,8 @@
+<?php
+
+$is_it_hp_brand            = true; // Data types Boolean
+$number_of_monitors        = 3; // Data type INT
+$is_monitors_condition_new = False; // Data type Boolean
+$display_size              = 15.6; // Data type Float/double
+$has_vga_cable             = true; // Data type boolean
+$sell_price                = 8000; // Data type INT
