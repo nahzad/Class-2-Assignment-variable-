e1fb635b86bd4b9ac9d71bd93a85fc55e02692a6
Nahzad Faruk <100996758+nahzad@users.noreply.github.com>
2022-03-25 00:42:54 +0600

    Case study added

--- find_variable.php
+++ find_variable.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+Case Study:
+
+I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+
+ */
+
 $is_it_hp_brand            = true; // Data types Boolean
 $number_of_monitors        = 3; // Data type INT
 $is_monitors_condition_new = False; // Data type Boolean
