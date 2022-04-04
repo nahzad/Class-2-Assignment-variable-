9b1dc46d94e7356b331cc9facb443fe243a197ec
Nahzad Faruk <100996758+nahzad@users.noreply.github.com>
2022-04-04 19:38:24 +0600

    Class 04 Assignment done

---
+++ Assignment Class04/AssignmentClass4.php
@@ -0,0 +1,96 @@
+<?php
+
+/* Question 01:
+
+Your client said that he needs a dropdown, which contains all days of the running month. Sometimes we fill up a registration form that, which date of birth. So find out in google (get number of days in specific month php) and do this assignment using a for loop.
+
+Day quantity is not equal for every month. Like as - february - 28, March - 31, april - 30
+
+So days of running month should dynamic (by php build in function)
+ */
+
+$current_year          = date( "Y" );
+$current_month         = date( "F" );
+$days_in_current_month = date( "t" );
+
+echo "Current Year is : " . $current_year . "<br>";
+echo "Running Month is : " . $current_month . "<br>";
+echo "Days in this current Month: " . $days_in_current_month . "<br><br>";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Showing Date Of Birth</title>
+</head>
+
+<body>
+    <label>
+        <h3>Select Date Of This Month</h3>
+    </label>
+    <select name="DOB" id="dob">
+        <?php
+for ( $i = 1; $i <= date( "t" ); $i++ ) {
+    echo '<option>' . 'Day No - ' . $i . '</option>';
+}
+?>
+</body>
+
+</html>
+<?php
+/* Question 02:
+If we print 1-100, our output will be 100 times. But this time you have print out only the odd (বিজোড়) number within 1-100.
+You have to rotate from 1 to 100.
+Your output message should like that, “3 is a odd number”
+Not need to print even number
+ */
+
+for ( $i = 0; $i <= 100; $i++ ) {
+
+    if ( $i % 2 !== 0 ) {
+        echo "$i is odd number" . "<br>";
+    }
+
+}
+
+/* Question 03:
+Factorial - I think everyone knows the word. Factorial of 4 is given below:
+4! = 4*3*2*1 = 24
+6! = 6*5*4*3*2*1 = 720
+So let’s calculate the factorial of 7. */
+
+$num = 7;
+
+$fact = 1;
+
+for ( $i = 1; $i <= $num; $i++ ) {
+    $fact = $fact * $i;
+}
+echo "factorial of $num is = " . $fact;
+
+/* Question 04:
+print out form 100 -1 using a while loop.
+You must use decrement operator */
+
+for ( $i = 100; $i > 0; $i-- ) {
+    echo $i . "<br>";
+}
+
+/*Question No 05:
+- ['apple','orange', 'banana','mango']
+- show all products as table list.
+- Output should like that: "sl - 1 and product - apple"
+
+ */
+
+$arr = ['sl-1' => 'apple', 'sl-2' => 'orange', 'sl-3' => 'banana', 'sl-4' => 'mango'];
+
+foreach ( $arr as $key => $products ) {
+    echo "$key and product -" . $products . "<br>";
+}
+
+?>
