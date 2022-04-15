7b6afaf5e91a7fdb3869cd0d900262e25985b793
Nahzad Faruk <100996758+nahzad@users.noreply.github.com>
2022-04-16 03:08:05 +0600

    some commnet added

--- Assignment Class 06/AssignmentClass06.php
+++ Assignment Class 06/AssignmentClass06.php
@@ -19,7 +19,7 @@ array('name' => 'orange', 'qty' => 7, 'total_price' => 600),
 How much product quantity you have purchased (total purchased qty = 67)
 
 Calculate the total amount of all purchased items (total amount = 2650 tk).*/
-echo "<h3>Question  : 01</h3>";
+echo "<h3>Question  : 01</h3>"; // for showing Question no in output page
 
 $purchased_products = [
 
@@ -33,16 +33,17 @@ $purchased_products = [
 
 foreach ( $purchased_products as $key => $value ) {
 
-    echo ++$key . "  ";
+    echo ++$key . "  "; // pre increament key, for not showing 0 index in result page
 
     foreach ( $value as $col ) {
         echo "--" . $col . " ";
     }
-    echo "<br>";
+    echo "<br>"; // for showing row in next line
 }
 
-echo "--------------------------<br>";
+echo "--------------------------<br>"; // just showing a horizontal line below the array result
 
+// Using array column function for merging those targeted key elements of above array
 echo " total purchased qty = " . array_sum( array_column( $purchased_products, 'qty' ) ) . " , ";
 
 echo " total amount = " . array_sum( array_column( $purchased_products, 'total_price' ) );
@@ -63,13 +64,17 @@ You have to make a single array from this two arrays
 
 And don’t keep any value in multiple time*/
 
-echo "<h3>Question  : 02</h3>";
+echo "<h3>Question  : 02</h3>"; // for showing Question no in output page
 
 $first_email_array = ["abc@gmail.com", "xyz@gmail.com", "demo@gmail.com"];
 
 $second_email_array = ["efg@gmail.com", "xyz@gmail.com", "dummy@gmail.com"];
 
+// Array merge for combined two array and it returns a new array
+
 $new_Merged_array = array_merge( $first_email_array, $second_email_array );
+
+// Array unique filtered out duplicate elements from $new_Merged_array
 
 $filtered_array = array_unique( $new_Merged_array );
 
@@ -87,9 +92,10 @@ Question 03:
 
 Split this given array into chunks 4*/
 
-echo "<h3>Question  : 03</h3>";
+echo "<h3>Question  : 03</h3>"; // for showing Question no in output page
 
 $citylist = ["Black Canyon City", "Chandler", "Flagstaff", "Gilbert", "Glendale", "Globe", "Mesa", "Miami", "Phoenix", "Peoria", "Prescott", "Scottsdale", "Sun City", "Surprise", "Tempe", "Tucson", "Wickenburg"];
+// Using array chunk function for spliting array
 
 $newCityList = array_chunk( $citylist, 4 );
 
@@ -99,7 +105,7 @@ foreach ( $newCityList as $key => $value ) {
 
     foreach ( $value as $city ) {
 
-        echo "<li>" . $city . "</li>"; //for only better presentation I use li tag.
+        echo "<li>" . $city . "</li>"; //for only better presentation I use <li> tag.
     }
 }
 
